4f4077f74f332e049be1350d865c1c7a1239d5f1
Tian Feng <feng.txy@outlook.com>
2022-05-13 17:50:16 -0700

    Update 1032 · Letter Case Permutation.py

--- Meta/1032 · Letter Case Permutation.py
+++ Meta/1032 · Letter Case Permutation.py
@@ -10,29 +10,17 @@ class Solution:
         self.dfs(s, 0, [], result)
         return result
 
-
     def dfs(self, s, index, path, result):
-        if index >= len(s) and len(path) == len(s):
+        if len(path) == len(s):
             result.append(''.join(path))
             return
 
-        for i in range(index, len(s)):
-            # if s[i].isdigit():
-            #     path.append(s[i])
-            #     self.dfs(s, i + 1, path, result)
-            #     path.pop()
-            # else:   
-            #     for char in [s[i].lower(), s[i].upper()]:
-            #         path.append(char)
-            #         self.dfs(s, i + 1, path, result)
-            #         path.pop()
+        if not s[index].isdigit():
+            next_list = [s[index].lower(), s[index].upper()]
+        else:
+            next_list = [s[index]]
 
-            if not s[i].isdigit():
-                next_list = [s[i].lower(), s[i].upper()]
-            else:
-                next_list = [s[i]]
-
-            for char in next_list:
-                    path.append(char)
-                    self.dfs(s, i + 1, path, result)
-                    path.pop()
+        for char in next_list:
+            path.append(char)
+            self.dfs(s, index + 1, path, result)
+            path.pop()
