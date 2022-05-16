8dee4d119c6baf149ae7b4023f02d1191a51e266
Tian Feng <feng.txy@outlook.com>
2022-05-16 00:01:17 -0700

    Update 911 · Maximum Size Subarray Sum Equals k.py

--- Meta/911 · Maximum Size Subarray Sum Equals k.py
+++ Meta/911 · Maximum Size Subarray Sum Equals k.py
@@ -6,18 +6,19 @@ class Solution:
     """
     def max_sub_array_len(self, nums, k):
         # Write your code here
+        prefix_index = {0:-1}#case when k == sum
         prefix_sum = 0
         ans = 0
-        sum2ind = dict()
         for i in range(len(nums)):
-            prefix_sum = prefix_sum + nums[i]
+            prefix_sum += nums[i]
+            diff = prefix_sum - k 
+            if prefix_sum not in prefix_index:
+                prefix_index[prefix_sum] = i
+                
+            if diff in prefix_index:
+                ans = max(ans, i - prefix_index[diff])
 
-            if prefix_sum not in sum2ind:
-                sum2ind[prefix_sum] = i
-            if prefix_sum == k:
-                ans = max(ans, i + 1)
-
-            if prefix_sum - k in sum2ind:
-                ans = max(ans, i - sum2ind[prefix_sum - k])
-
+            
         return ans 
+
+
