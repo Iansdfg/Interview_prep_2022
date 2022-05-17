effb65a9731f29db7285a56a59c1d07ca3a74af0
Tian Feng <feng.txy@outlook.com>
2022-05-16 21:10:20 -0700

    Update 100 · Remove Duplicates from Sorted Array.py

--- Meta/100 · Remove Duplicates from Sorted Array.py
+++ Meta/100 · Remove Duplicates from Sorted Array.py
@@ -5,7 +5,8 @@ class Solution:
     """
     def removeDuplicates(self, nums):
         # write your code here
-        index = 0
+        if not nums:
+            return 0
         for i in range(len(nums)-1, 0, -1):
             if nums[i] == nums[i-1]:
                 nums.pop(i)
